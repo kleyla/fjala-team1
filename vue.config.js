04a753f95711b4017a924794b55a1aa93f1d597c
kleyla <kleyla01@live.com>
2020-11-18 01:55:00 -0400

    deploy fixed

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/fjala-team1/" : "/",
+  transpileDependencies: ["vuetify"],
+};
